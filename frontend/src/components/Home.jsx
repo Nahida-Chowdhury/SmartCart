2542c20e869518cf0c781cfd6d2a6a8c70703292
Nahida-Chowdhury <chowdhurynahida06@gmail.com>
2025-08-22 18:50:20 +0600

    Update Home and Shop components, replace images, add common components

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import LatestProducts from './common/LatestProducts';
+import FeaturedProducts from './common/FeaturedProducts';
+import Hero from './common/Hero';
+import Layout from './common/Layout';
 
 const Home = () => {
   return (
-    <div>
-      <header className='shadow'>
-        <button className='btn btn-primary'>
-          Click Me
-        </button>
-      </header>
-    </div>
+    <>
+      <Layout>
+        <Hero />
+        <LatestProducts />
+        <FeaturedProducts />
+      </Layout>
+    </>
   )
 }
 
