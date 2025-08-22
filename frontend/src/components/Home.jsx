e29f27baf51a3550800695b9c88e4d60f498f71e
Nahida-Chowdhury <chowdhurynahida06@gmail.com>
2025-08-22 14:31:31 +0600

    first commit

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <header className='shadow'>
+        <button className='btn btn-primary'>
+          Click Me
+        </button>
+      </header>
+    </div>
+  )
+}
+
+export default Home
