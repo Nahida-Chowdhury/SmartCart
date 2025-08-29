5d689c64f33adec2921adce670a12468bbe7017d
Nahida-Chowdhury <chowdhurynahida06@gmail.com>
2025-08-29 15:20:04 +0600

    Updated product Create and Show components

--- frontend/src/components/admin/product/Show.jsx
+++ frontend/src/components/admin/product/Show.jsx
@@ -33,6 +33,27 @@ const Show = () => {
                 }
             })
     }
+    const deleteProduct = async (id) => {
+        if (confirm("Are you sure you want to delete?")) {
+            const res = await fetch(`${apiUrl}/products/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${adminToken()}`
+                }
+            }).then(res => res.json())
+                .then(result => {
+                    if (result.status === 200) {
+                        const newProducts = products.filter(product => product.id != id)
+                        setProducts(newProducts)
+                        toast.success(result.message)
+                    } else {
+                        console.log("Something went wrong!")
+                    }
+                })
+        }
+    }
     useEffect(() => {
         fetchProducts()
     }, []);
@@ -76,11 +97,11 @@ const Show = () => {
                                         <tbody>
                                             {
                                                 products.map(product => {
-                                                    return(
-                                                        <th>
+                                                    return (
+                                                        <tr>
                                                             <td>{product.id}</td>
                                                             <td>
-                                                                <img src={product.image_url} alt="" width={50}/>
+                                                                <img src={product.image_url} alt="" width={50} />
                                                             </td>
                                                             <td>{product.title}</td>
                                                             <td>{product.price}</td>
@@ -93,7 +114,7 @@ const Show = () => {
                                                                 }
                                                             </td>
                                                             <td>
-                                                                <Link to={`/admin/categories/edit/${product.id}`} className='text-primary'>
+                                                                <Link to={`/admin/products/edit/${product.id}`} className='text-primary'>
                                                                     <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"></path></svg>
                                                                 </Link>
                                                                 <Link className='text-danger ms-2' onClick={() => deleteProduct(product.id)}>
@@ -102,7 +123,7 @@ const Show = () => {
                                                                     </svg>
                                                                 </Link>
                                                             </td>
-                                                        </th>
+                                                        </tr>
                                                     )
                                                 })
                                             }
