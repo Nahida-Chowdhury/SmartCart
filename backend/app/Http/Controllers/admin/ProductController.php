50a0d0ccbfe00d43467ef085a5cd47f1dd5743dd
Rimi-03 <chowdhuryrimi59@gmail.com>
2025-08-29 08:46:39 +0600

    Fetches product data from backend

---
+++ backend/app/Http/Controllers/admin/ProductController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    //return all brands
+    public function index()
+    {
+        $products = Product::orderBy('created_at', 'DESC')->get();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $products
+        ]);
+    }
+
+    //store  in db
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'price' => 'required|numeric',
+            'category_id' => 'required|integer',
+            'sku' => 'required|unique:products,sku,except,id',
+            'is_featured' => 'required',
+            'status' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $product = new Product();
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->compare_price = $request->compare_price;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+        $product->sku = $request->sku;
+        $product->qty = $request->qty;
+        $product->description = $request->description;
+        $product->short_description = $request->short_description;
+        $product->status = $request->status;
+        $product->is_featured = $request->is_featured;
+        $product->barcode = $request->barcode;
+        $product->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Product has been created successfully!',
+
+        ], 200);
+    }
+
+    //return a single Brand
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if ($product == null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'product not found',
+                'data' => []
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'data' => $product
+        ]);
+    }
+
+    //update a Brand
+    public function update($id, Request $request)
+    {
+
+        $product = Product::find($id);
+        if ($product == null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Product not found',
+                'data' => []
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'price' => 'required|numeric',
+            'category_id' => 'required|integer',
+            'sku' => 'required|unique:products,sku,$id,id',
+            'is_featured' => 'required',
+            'status' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->compare_price = $request->compare_price;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+        $product->sku = $request->sku;
+        $product->qty = $request->qty;
+        $product->description = $request->description;
+        $product->short_description = $request->short_description;
+        $product->status = $request->status;
+        $product->is_featured = $request->is_featured;
+        $product->barcode = $request->barcode;
+        $product->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Product has been updated successfully!',
+            'data' => $product
+
+        ], 200);
+    }
+
+    //delete a single Brand
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+
+        if ($product == null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Product not found',
+                'data' => []
+            ], 404);
+        }
+
+        $product->delete();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Product deleted successfully!',
+        ], 200);
+    }
+}
--- backend/database/migrations/2025_08_29_003542_create_products_table.php
+++ backend/database/migrations/2025_08_29_003542_create_products_table.php
@@ -20,9 +20,9 @@ return new class extends Migration
             $table->text('short_description')->nullable();
             $table->string('image')->nullable();
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
-            $table->foreignId('brand_id')->constrained()->onDelete('cascade');
+            $table->foreignId('brand_id')->nullable()->constrained()->onDelete('cascade');
             $table->integer('qty')->nullable();
-            $table->integer('sku');
+            $table->string('sku');
             $table->string('barcode')->nullable();
             $table->integer('status')->default(1);
             $table->enum('is_featured', ['yes', 'no'])->default('no');
