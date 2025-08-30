29dbf082b380d17d6a46872ae77e9b0054cb1145
Rimi-03 <chowdhuryrimi59@gmail.com>
2025-08-30 13:46:48 +0600

    Intitial comit

---
+++ frontend/src/components/Confirmation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "./common/Layout";
+
+const Confirmation = () => {
+  return (
+    <Layout>
+      <div className="container">
+        <div className="row">
+          <div></div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
