ac42571c62e4593476fde07cf2680e384bea35d3
Rimi-03 <chowdhuryrimi59@gmail.com>
2025-08-30 11:22:39 +0600

    Added customer authenticate and fetch data

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\ProductController;
 use App\Http\Controllers\admin\SizeController;
 use App\Http\Controllers\admin\TempImageController;
+use App\Http\Controllers\front\AccountController;
 use App\Http\Controllers\front\ProductController as FrontProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,6 +18,8 @@ Route::get('get-categories', [FrontProductController::class, 'getCategories']);
 Route::get('get-brands', [FrontProductController::class, 'getbrands']);
 Route::get('get-products', [FrontProductController::class, 'getProducts']);
 Route::get('get-product/{id}', [FrontProductController::class, 'getProduct']);
+Route::get('register', [AccountController::class, 'register']);
+Route::get('login', [AccountController::class, 'authenticate']);
 
 
 
