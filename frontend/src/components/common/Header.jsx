4263a2a5c7165f03b3142ffd5e64b182e977f20e
Nahida-Chowdhury <chowdhurynahida06@gmail.com>
2025-08-30 14:27:14 +0600

    Integrate API in Header.jsx

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../../assets/images/logo1.png';
 import { Link } from 'react-router-dom';
+import { apiUrl } from './http';
 
 const Header = () => {
+    const [categories, setCategories] = useState([]);
+
+    const fetchCategories = () => {
+        fetch(`${apiUrl}/get-categories`, {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json',
+            }
+        }).then(res => res.json())
+            .then(result => {
+                if (result.status === 200) {
+                    setCategories(result.data)
+                } else {
+                    console.log("Something went wrong!")
+                }
+
+            })
+    }
+
+    useEffect(() => {
+        fetchCategories()
+    }, [])
     return (
         <header className='shadow'>
             <div className='bg-dark text-center py-3'>
@@ -20,9 +44,13 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav className="ms-auto my-2 my-lg-0" navbarScroll>
-                            <Nav.Link href="#action1">Mens</Nav.Link>
-                            <Nav.Link href="#action2">Women</Nav.Link>
-                            <Nav.Link href="#action3">Kids</Nav.Link>
+                            {
+                                categories && categories.map(category => {
+                                    return(
+                                         <Nav.Link href={`/shop?category=${category.id}`}>{category.name}</Nav.Link>
+                                    )
+                                })
+                            }
                         </Nav>
                         <div className='nav-right d-flex'>
                             <a href="" className='ms-3'>
